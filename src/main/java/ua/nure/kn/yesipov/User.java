0c332146e8a818cf478ab314321e7625c4f5585a
Максим Єсіпов <maksym.yesipov@nure.ua>
2018-11-23 13:13:01 +0200

    com

--- src/main/java/ua/nure/kn/yesipov/User.java
+++ src/main/java/ua/nure/kn/yesipov/User.java
@@ -1,6 +1,5 @@
 package main.java.ua.nure.kn.yesipov;
 
-
 import java.util.Calendar;
 import java.util.Date;
 
@@ -13,39 +12,30 @@ public class User {
     public Date getDateOfBirthd() {
         return dateOfBirthd;
     }
-
     public void setDateOfBirthd(Date dateOfBirthd) {
         this.dateOfBirthd = dateOfBirthd;
     }
-
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public Object getFullName() {
         return getLastName() + ", " + getFirstName();
     }
-
     public int getAge() {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(new Date());
