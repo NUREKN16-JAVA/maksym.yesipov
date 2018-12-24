45aae9e058c6986af06b89b5ef381c66893f37d3
MaksymYes <maksym.yesipov@nure.ua>
2018-12-24 12:13:51 +0200

    MockServletTestCase class created

---
+++ src/Test/java/ua/nure/kn/yesipov/web/MockServletTestCase.java
@@ -0,0 +1,36 @@
+package Test.java.ua.nure.kn.yesipov.web;
+
+import Test.java.ua.nure.kn.yesipov.database.MockDaoFactory;
+import com.mockobjects.dynamic.Mock;
+import com.mockrunner.servlet.BasicServletTestCaseAdapter;
+import main.java.ua.nure.kn.yesipov.database.DaoFactory;
+
+import java.util.Properties;
+
+public abstract class MockServletTestCase extends BasicServletTestCaseAdapter {
+    private Mock mockUserDao;
+
+    public Mock getMockUserDao() {
+        return mockUserDao;
+    }
+
+    public void setMockUserDao(Mock mockUserDao) {
+        this.mockUserDao = mockUserDao;
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        Properties properties = new Properties();
+
+        properties.setProperty("dao.factory", MockDaoFactory.class.getName());
+
+        DaoFactory.init(properties);
+
+        setMockUserDao(((MockDaoFactory) DaoFactory.getInstance()).getMockUserDao());
+    }
+
+    public void tearDown() throws Exception {
+        getMockUserDao().verify();
+        super.tearDown();
+    }
+}
