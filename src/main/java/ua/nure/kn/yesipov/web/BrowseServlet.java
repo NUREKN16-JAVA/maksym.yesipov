3c40cf0bfd476adfc274a0426dfe97a3c54ff2a6
MaksymYes <maksym.yesipov@nure.ua>
2018-12-24 13:05:07 +0200

    BrowseServlet implementations

--- src/main/java/ua/nure/kn/yesipov/web/BrowseServlet.java
+++ src/main/java/ua/nure/kn/yesipov/web/BrowseServlet.java
@@ -1,8 +1,32 @@
 package main.java.ua.nure.kn.yesipov.web;
 
+import main.java.ua.nure.kn.yesipov.User;
+import main.java.ua.nure.kn.yesipov.database.DaoFactory;
+import main.java.ua.nure.kn.yesipov.database.DatabaseException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
 
 public class BrowseServlet extends HttpServlet {
 
+    private static final String BROWSE_JSP = "/browse.jsp";
 
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        browse(req, resp);
+    }
+
+    private void browse(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+        try {
+            Collection<User> users = DaoFactory.getInstance().getUserDao().findAll();
+            req.getSession(true).setAttribute("users", users);
+            req.getRequestDispatcher(BROWSE_JSP).forward(req, resp);
+        } catch (DatabaseException | IOException e) {
+            throw new ServletException(e);
+        }
+    }
 }
