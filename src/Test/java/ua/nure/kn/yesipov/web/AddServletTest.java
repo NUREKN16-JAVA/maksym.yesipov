40be2881024cdd7aefcfdbea5a630d4b68a823bc
MaksymYes <maksym.yesipov@nure.ua>
2018-12-26 04:57:58 +0200

    AddServlet main & test classes created

---
+++ src/Test/java/ua/nure/kn/yesipov/web/AddServletTest.java
@@ -0,0 +1,74 @@
+package Test.java.ua.nure.kn.yesipov.web;
+
+import main.java.ua.nure.kn.yesipov.User;
+import main.java.ua.nure.kn.yesipov.web.AddServlet;
+import org.junit.Before;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+public class AddServletTest extends MockServletTestCase {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        createServlet(AddServlet.class);
+    }
+
+    public void testAdd() {
+        Date date = new Date();
+        User newUser = new User("John", "Doe", date);
+        User user = new User(new Long(1000), "John", "Doe", date);
+        getMockUserDao().expectAndReturn("create", newUser, user);
+
+        addRequestParameter("firstName", "John");
+        addRequestParameter("lastName", "Doe");
+        addRequestParameter("date", DateFormat.getDateInstance().format(date));
+        addRequestParameter("okButton", "Ok");
+
+        doPost();
+    }
+
+    public void testAddEmptyFirstName() {
+        Date date = new Date();
+        addRequestParameter("lastName", "Doe");
+        addRequestParameter("date", DateFormat.getDateInstance().format(date));
+        addRequestParameter("okButton", "Ok");
+        doPost();
+
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session", errorMessage);
+    }
+
+    public void testAddEmptyLastName() {
+        Date date = new Date();
+        addRequestParameter("firstName", "John");
+        addRequestParameter("date", DateFormat.getDateInstance().format(date));
+        addRequestParameter("okButton", "Ok");
+        doPost();
+
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session", errorMessage);
+    }
+
+    public void testAddEmptyDate() {
+        addRequestParameter("firstName", "John");
+        addRequestParameter("lastName", "Doe");
+        addRequestParameter("okButton", "Ok");
+        doPost();
+
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session", errorMessage);
+    }
+
+    public void testAddInvalidDate() {
+        addRequestParameter("firstName", "John");
+        addRequestParameter("lastName", "Doe");
+        addRequestParameter("date", "asdasdaewqe");
+        addRequestParameter("okButton", "Ok");
+
+        doPost();
+        String errorMessage = (String) getWebMockObjectFactory().getMockRequest().getAttribute("error");
+        assertNotNull("Could not find error message in session", errorMessage);
+    }
+}
---
+++ src/main/java/ua/nure/kn/yesipov/web/AddServlet.java
@@ -0,0 +1,7 @@
+package main.java.ua.nure.kn.yesipov.web;
+
+import javax.servlet.http.HttpServlet;
+
+public class AddServlet extends HttpServlet {
+
+}
