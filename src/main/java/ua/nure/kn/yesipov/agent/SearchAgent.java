c26d113df57213f5f7a74164c3e782e3c6e12536
MaksymYes <maksym.yesipov@nure.ua>
2018-12-27 16:51:51 +0200

    setup and tearDown implementations

--- src/main/java/ua/nure/kn/yesipov/agent/SearchAgent.java
+++ src/main/java/ua/nure/kn/yesipov/agent/SearchAgent.java
@@ -2,6 +2,10 @@ package main.java.ua.nure.kn.yesipov.agent;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import main.java.ua.nure.kn.yesipov.User;
 import main.java.ua.nure.kn.yesipov.database.DaoFactory;
 import main.java.ua.nure.kn.yesipov.database.DatabaseException;
@@ -15,12 +19,33 @@ public class SearchAgent extends Agent {
     protected void setup() {
         super.setup();
         System.out.println(getAID().getName() + " agent started");
+
+        addBehaviour(new RequestServer());
+
+        DFAgentDescription description = new DFAgentDescription();
+        description.setName(getAID());
+        ServiceDescription serviceDescription = new ServiceDescription();
+        serviceDescription.setName("JADE-searching");
+        serviceDescription.setType("searching");
+        description.addServices(serviceDescription);
+
+        try {
+            DFService.register(this, description);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void takeDown() {
         super.takeDown();
         System.out.println(getAID().getName() + " agent finished");
+
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+        }
     }
 
     public void search(String firstName, String lastName) throws SearchException {
