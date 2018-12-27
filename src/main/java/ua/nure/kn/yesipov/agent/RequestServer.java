3015910eab1a9244b11ad9af9152fff08607c0b3
MaksymYes <maksym.yesipov@nure.ua>
2018-12-27 15:59:27 +0200

    parseMessage and createReply methods implementation

--- src/main/java/ua/nure/kn/yesipov/agent/RequestServer.java
+++ src/main/java/ua/nure/kn/yesipov/agent/RequestServer.java
@@ -3,15 +3,18 @@ package main.java.ua.nure.kn.yesipov.agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 import main.java.ua.nure.kn.yesipov.User;
+import main.java.ua.nure.kn.yesipov.database.DaoFactory;
+import main.java.ua.nure.kn.yesipov.database.DatabaseException;
 
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
+
+import static java.util.Objects.nonNull;
 
 public class RequestServer extends CyclicBehaviour {
     @Override
     public void action() {
         ACLMessage message = myAgent.receive();
-        if (Objects.nonNull(message)) {
+        if (nonNull(message)) {
             if (message.getPerformative() == ACLMessage.REQUEST) {
                 myAgent.send(createReply(message));
             } else {
@@ -24,10 +27,50 @@ public class RequestServer extends CyclicBehaviour {
     }
 
     private Collection<User> parseMessage(ACLMessage message) {
+        Collection<User> users = new LinkedList<>();
 
+        String content = message.getContent();
+        if (nonNull(content) && !content.isEmpty()) {
+            StringTokenizer tokenizer1 = new StringTokenizer(content, ";");
+            while (tokenizer1.hasMoreTokens()) {
+                String userInfo = tokenizer1.nextToken();
+                StringTokenizer tokenizer2 = new StringTokenizer(userInfo, ",");
+                String id = tokenizer2.nextToken();
+                String firstName = tokenizer2.nextToken();
+                String lastName = tokenizer2.nextToken();
+                users.add(new User(new Long(id), firstName, lastName, null));
+            }
+        }
+        return users;
     }
 
     private ACLMessage createReply(ACLMessage message) {
-        
+        ACLMessage reply = message.createReply();
+        reply.setPerformative(ACLMessage.INFORM);
+        String content = message.getContent();
+
+        if (nonNull(content) && !content.isEmpty()) {
+            StringTokenizer tokenizer = new StringTokenizer(content, ",");
+            if (tokenizer.countTokens() == 2) {
+                String firstName = tokenizer.nextToken();
+                String lastName = tokenizer.nextToken();
+                Collection<User> users = null;
+                try {
+                    users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+                } catch (DatabaseException e) {
+                    e.printStackTrace();
+                    users = Collections.emptyList();
+                }
+                StringBuffer buffer = new StringBuffer();
+                for (Iterator it = users.iterator(); it.hasNext(); ) {
+                    User user = (User) it.next();
+                    buffer.append(user.getId()).append(",")
+                            .append(user.getFirstName()).append(",")
+                            .append(user.getLastName()).append(";");
+                }
+                reply.setContent(buffer.toString());
+            }
+        }
+        return reply;
     }
 }
